7bb8183f501e2f9e8270bd4a80c581f2413276b3
Luke Burri <LBUrri@lcs.on.ca>
2020-11-26 08:57:08 -0500

    Initial Commit

---
+++ BowWow/BowWowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowWowApp.swift
+//  BowWow
+//
+//  Created by Burri, Luke on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct BowWowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
